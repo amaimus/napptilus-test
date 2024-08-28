d41333b58535518a3ca085fdacb0a7ceb38bf196
alejandro <alejandromaimus147@gmail.com>
2024-08-28 22:12:09 +0200

    Header styling & functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,22 @@ import { Navigate, Route, Routes, Link } from 'react-router-dom'
 
 import EmployeesList from './components/EmployeesList' 
 import EmployeeDetails from './components/EmployeeDetails' 
+import Header from './components/Header' 
 
 
 
 function App() {
 
   return (
-    <>
-      <header>
-        <Link to={`/employees`}>Oompa Loompa's Crew</Link>
-      </header>
-
-
+    <body className='max-w-screen-xl mx-auto'>
+      <Header />
+      
       <Routes>
         <Route path='/' element={<Navigate to='/employees' />} />
         <Route path='/employees' element={<EmployeesList />} />
         <Route path='/employees/:employeeId' element={<EmployeeDetails />} />
       </Routes>        
-    </>
+    </body>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import avatarImage from '../assets/avatar.png'
+
+function EmployeesList() {
+
+  return (
+    <header className='bg-gray-300'>
+      <ul className='px-16 py-4'>
+        <li className='flex gap-8'>
+          <img src={avatarImage} className='w-8' alt='Vite logo' />
+          <Link to={`/employees`} className='font-bold'>Oompa Loompa's Crew</Link>
+        </li>
+      </ul>
+    </header>
+  )
+}
+
+export default EmployeesList
